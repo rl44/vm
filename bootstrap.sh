574a51f96fff7a3da718d2306aeb235f611a3358
Remi Lehn <remi.lehn@polytech.univ-nantes.fr>
2012-01-13 17:24:18 +0100

    Initialisation de bootstrap.sh (il n'y a rien dedans pour l'instant).

---
+++ bootstrap.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+#
+
+# Ce script est similaire à un script configure dans ses objectifs. En plus
+# d'un script configure (généré par autoconf), il a pour objectif de permettre
+# une installation d'un environnement fonctionnel de manière autonome, sans
+# nécessiter la présence d'autre chose que ce script.
+#
+# Il peut être exécuté directement sous une forme :
+#
+#             wget -q -O- URL | sh
+#
+#                    ou
+#
+#             curl -L URL | sh
+#
+
+
